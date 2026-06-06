d1be6611271389d82e94cce49aa8bd8e7ffd03fd
Willis Dyson <willisdyson@gmail.com>
2026-06-06 20:35:00 +0100

    Swiper package init for carousel

--- lcc-booking-system-app/src/components/day-selector/day-selector-carousel/DaySelectorCarousel.tsx
+++ lcc-booking-system-app/src/components/day-selector/day-selector-carousel/DaySelectorCarousel.tsx
@@ -1,17 +1,41 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { A11y, Navigation, Pagination, Scrollbar } from 'swiper/modules';
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 import DaySelectorCarouselItem from "./day-selector-carousel-item/DaySelectorCarouselItem";
 import styles from "./DaySelectorCarousel.module.scss";
 
 const DaySelectorCarousel = ({ }) => {
     return (
-        <div className={styles["day-selector-carousel"]}>
-            <DaySelectorCarouselItem active={true} />
-            <DaySelectorCarouselItem />
-            <DaySelectorCarouselItem />
-            <DaySelectorCarouselItem />
-            <DaySelectorCarouselItem />
-            <DaySelectorCarouselItem />
-            <DaySelectorCarouselItem />
-        </div>
+        <Swiper
+        className={styles["day-selector-carousel"]}
+            modules={[Navigation, Pagination, Scrollbar, A11y]}
+            spaceBetween={0}
+            slidesPerView={1}
+            navigation>
+                {/* TODO: Add dynamic looping to generate slides */}
+            <SwiperSlide className={styles["day-selector-carousel__slide"]}>
+                <DaySelectorCarouselItem active />
+                <DaySelectorCarouselItem/>
+                <DaySelectorCarouselItem />
+                <DaySelectorCarouselItem />
+                <DaySelectorCarouselItem />
+                <DaySelectorCarouselItem />
+                <DaySelectorCarouselItem />
+            </SwiperSlide>
+            <SwiperSlide className={styles["day-selector-carousel__slide"]}>
+                <DaySelectorCarouselItem />
+                <DaySelectorCarouselItem />
+                <DaySelectorCarouselItem />
+                <DaySelectorCarouselItem />
+                <DaySelectorCarouselItem />
+                <DaySelectorCarouselItem />
+                <DaySelectorCarouselItem />
+            </SwiperSlide>
+        </Swiper>
     );
 };
 
