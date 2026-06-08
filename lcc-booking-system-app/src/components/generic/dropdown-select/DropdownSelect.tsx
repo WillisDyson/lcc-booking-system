9a0f910cdd6cdd482eb7123c85e97c420d0518b6
Willis Dyson <willisdyson@gmail.com>
2026-06-08 12:13:13 +0100

    Dropdown select open/close functionality

--- lcc-booking-system-app/src/components/generic/dropdown-select/DropdownSelect.tsx
+++ lcc-booking-system-app/src/components/generic/dropdown-select/DropdownSelect.tsx
@@ -1,44 +1,75 @@
+import { useId, useState } from "react";
 import styles from "./DropdownSelect.module.scss";
 
 const DropdownSelect = ({ dropdownText }: { dropdownText: string }) => {
+    const [isOpen, setIsOpen] = useState(false);
+    const panelId = useId();
+
     return (
-        <div className={styles["dropdown-select"]}>
-            <button type="button" className={styles["dropdown-select__button"]}>
+        <div
+            className={`${styles["dropdown-select"]} ${isOpen ? styles["dropdown-select--open"] : ""}`}
+            onKeyDown={(e) => {
+                if (e.key === "Escape") {
+                    setIsOpen(false);
+                }
+            }}
+            onBlur={(e) => {
+                if (!e.currentTarget.contains(e.relatedTarget as Node)) {
+                    setIsOpen(false);
+                }
+            }}
+        >
+            <button
+                type="button"
+                className={styles["dropdown-select__button"]}
+                onClick={() => setIsOpen((previousState) => !previousState)}
+                onFocus={(e) => {
+                    if (e.currentTarget.matches(":focus-visible")) {
+                        setIsOpen(true);
+                    }
+                }}
+                aria-expanded={isOpen}
+                aria-controls={panelId}
+            >
                 <span className={styles["dropdown-select__button-text"]}>{dropdownText}</span>
                 <span className={styles["dropdown-select__arrow"]}></span>
             </button>
 
             {/* TODO: Add dynamic link generation */}
             
-            <fieldset className={styles["dropdown-select__content"]}>
-                <label className={styles["dropdown-select__label"]}>
+            <fieldset
+                id={panelId}
+                className={`${styles["dropdown-select__content"]} `}
+                onMouseDown={(e) => e.preDefault()} // Pres the dropdown from closing when on a <label> or on space between options
+            >
+                <label className={styles["dropdown-select__option"]}>
                     <input className={styles["dropdown-select__checkbox"]}type="checkbox" name="Filter" value="Swim"/>
-                    <span className={styles["dropdown-select__custom-checkbox"]}></span> Swim
+                    Swim
                 </label>
 
-                <label className={styles["dropdown-select__label"]}>
+                <label className={styles["dropdown-select__option"]}>
                     <input className={styles["dropdown-select__checkbox"]} type="checkbox" name="Filter" value="Fitness"/>
-                    <span className={styles["dropdown-select__custom-checkbox"]}></span> Fitness
+                    Fitness
                 </label>
 
-                <label className={styles["dropdown-select__label"]}>
+                <label className={styles["dropdown-select__option"]}>
                     <input className={styles["dropdown-select__checkbox"]} type="checkbox" name="Filter" value="Yoga"/>
-                    <span className={styles["dropdown-select__custom-checkbox"]}></span> Yoga
+                    Yoga
                 </label>
 
-                <label className={styles["dropdown-select__label"]}>
+                <label className={styles["dropdown-select__option"]}>
                     <input className={styles["dropdown-select__checkbox"]} type="checkbox" name="Filter" value="Zumba"/>
-                    <span className={styles["dropdown-select__custom-checkbox"]}></span> Zumba
+                    Zumba
                 </label>
 
-                <label className={styles["dropdown-select__label"]}>
+                <label className={styles["dropdown-select__option"]}>
                     <input className={styles["dropdown-select__checkbox"]} type="checkbox" name="Filter" value="Health Programmes"/>
-                    <span className={styles["dropdown-select__custom-checkbox"]}></span> Health Programmes
+                    Health Programmes
                 </label>
 
-                <label className={styles["dropdown-select__label"]}>
+                <label className={styles["dropdown-select__option"]}>
                     <input className={styles["dropdown-select__checkbox"]} type="checkbox" name="Filter" value="Lane Swim" />
-                    <span className={styles["dropdown-select__custom-checkbox"]}></span>Lane Swim
+                    Lane Swim
                 </label>
             </fieldset>
         </div>
